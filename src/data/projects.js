8dfef4c2d6792fe013bb616c0da9073b14b44a76
Macarena Alonso <alonsomacarena@gmail.com>
2025-01-28 18:00:20 +0100

    changes styles and images

---
+++ src/data/projects.js
@@ -0,0 +1,64 @@
+export const projects = [
+    {
+      id: 'canal-hispanidad',
+      title: 'Canal Hispanidad',
+      description: '',
+      img: '/assets/hispanidad.png',
+      img_alt: 'Canal Hispanidad',
+      skills: [
+        'ReactJs',
+        //'Redux',
+        //'React-Router',
+        //'Vite',
+        'VideoJs',
+        'Stripe',
+        'MedusaJs'
+      ],
+      url: 'https://canalhispanidad.com/',
+      publishDate: new Date('2024-11-11'),
+    },
+    {
+      id: 'movistar-argentina',
+      title: 'Movistar Argentina',
+      description: '',
+      img: '/assets/movistar-argentina.png',
+      img_alt: 'Movistar Argentina',
+      skills: [
+        'HTML',
+        'CSS',
+        'Javascript'
+      ],
+      url: 'https://movistarinternet.ar/p/td',
+      publishDate: new Date('2024-08-08'),
+    },
+    {
+      id: 'mapa',
+      title: 'Mapa de Aguas',
+      description: '',
+      img: '/assets/mapa.png',
+      img_alt: 'Mapa de Aguas',
+      skills: [
+        'HTML',
+        'CSS',
+        'Javascript',
+        'PHP'
+      ],
+      url: 'https://www.mapadeaguas.com/',
+      publishDate: new Date('2024-08-08'),
+    },
+    {
+      id: 'movistar-argentina',
+      title: 'Movistar Argentina',
+      description: '',
+      img: '/assets/movistar-argentina.png',
+      img_alt: 'Movistar Argentina',
+      skills: [
+        'HTML',
+        'CSS',
+        'Javascript'
+      ],
+      url: 'https://movistarinternet.ar/p/td',
+      publishDate: new Date('2024-08-08'),
+    },
+];
+  
