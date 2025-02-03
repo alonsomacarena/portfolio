6f4bf0e7f7eb8e6782e666fc71248cbeda82b54e
Macarena Alonso <alonsomacarena@gmail.com>
2025-02-03 17:04:29 +0100

    add new projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,9 +2,10 @@ export const projects = [
     {
       id: 'canal-hispanidad',
       title: 'Canal Hispanidad',
-      description: '',
-      img: '/assets/hispanidad.png',
+      description: 'Canal Hispanidad es una plataforma dedicada a la compra y alquiler de documentales y películas producidas por Lopez-Li Film. El proyecto fue desarrollado utilizando React, aprovechando su sistema de componentes reutilizables para garantizar una experiencia de usuario fluida. Además, se implementaron tecnologías como VideoJs para la reproducción de videos, Stripe para el procesamiento de pagos y MedusaJs como solución de backend para gestionar productos y pagos. La plataforma está optimizada para ofrecer una navegación intuitiva y un proceso de compra rápido.',
+      img: '/assets/hispanidad/hispanidad.png',
       img_alt: 'Canal Hispanidad',
+      img_mockup: '/assets/hispanidad/hispanidad-mockup.png',
       skills: [
         'ReactJs',
         //'Redux',
@@ -20,23 +21,25 @@ export const projects = [
     {
       id: 'movistar-argentina',
       title: 'Movistar Argentina',
-      description: '',
-      img: '/assets/movistar-argentina.png',
+      description: 'Para Movistar Argentina, creé una página web orientada a captar leads y generar ventas de servicios de internet y móvil. Usé HTML, CSS y JavaScript para desarrollar un diseño optimizado para la conversión. Los formularios son fáciles de completar, y la experiencia está pensada para que los usuarios puedan encontrar lo que buscan rápidamente.',
+      img: '/assets/movistar-argentina/movistar-argentina.png',
       img_alt: 'Movistar Argentina',
+      img_mockup: '/assets/movistar-argentina/movistar-argentina-mockup.png',
       skills: [
         'HTML',
         'CSS',
         'Javascript'
       ],
       url: 'https://movistarinternet.ar/p/td',
-      publishDate: new Date('2024-08-08'),
+      publishDate: new Date('2024-10-10'),
     },
     {
-      id: 'mapa',
+      id: 'mapa-de-aguas',
       title: 'Mapa de Aguas',
-      description: '',
-      img: '/assets/mapa.png',
+      description: 'Mapa de Aguas es un sitio informativo creado para Javier Domínguez Angulo, escritor y diseñador de paisajes. El sitio consta de varias páginas donde los usuarios pueden conocer su trayectoria, explorar sus proyectos y acceder a información sobre su filosofía y su obra. El sitio está desarrollado con HTML, CSS y JavaScript, con un formulario de contacto implementado en PHP para facilitar la comunicación directa.',
+      img: '/assets/mapa/mapa.png',
       img_alt: 'Mapa de Aguas',
+      img_mockup: '/assets/mapa/mapa-mockup.png',
       skills: [
         'HTML',
         'CSS',
@@ -44,21 +47,52 @@ export const projects = [
         'PHP'
       ],
       url: 'https://www.mapadeaguas.com/',
-      publishDate: new Date('2024-08-08'),
+      publishDate: new Date('2024-09-09'),
     },
     {
-      id: 'movistar-argentina',
-      title: 'Movistar Argentina',
-      description: '',
-      img: '/assets/movistar-argentina.png',
-      img_alt: 'Movistar Argentina',
+      id: 'claro-costa-rica',
+      title: 'Claro Costa Rica',
+      description: 'En este proyecto para Claro Costa Rica, desarrollé una página web orientada a la venta de planes móviles y servicios de internet. A través de varias páginas, los usuarios pueden comparar las opciones disponibles, obtener detalles sobre los planes y enviar sus datos a través de formularios. El sitio fue realizado con HTML, CSS y JavaScript, con un diseño limpio que facilita la navegación y la conversión de usuarios en leads.',
+      img: '/assets/claro/claro.png',
+      img_alt: 'Claro Costa Rica',
+      img_mockup: '/assets/claro/claro-mockup.png',
       skills: [
         'HTML',
         'CSS',
         'Javascript'
       ],
-      url: 'https://movistarinternet.ar/p/td',
+      url: 'https://claroplanes.cr/p/preview',
       publishDate: new Date('2024-08-08'),
+    },
+    {
+      id: 'movistar-chile',
+      title: 'Movistar Chile',
+      description: 'El sitio web para Movistar Chile está diseñado para captar leads y fomentar la venta de planes móviles y servicios de hogar. El sitio incluye varias páginas con comparativas de planes, formularios de registro y llamadas a la acción claras para facilitar la conversión. Usé HTML, CSS y JavaScript para desarrollar un sitio rápido y funcional, ofreciendo una interfaz clara y accesible en dispositivos móviles y desktop.',
+      img: '/assets/movistar-chile/movistar-chile.png',
+      img_alt: 'Movistar Chile',
+      img_mockup: '/assets/movistar-chile/movistar-chile-mockup.png',
+      skills: [
+        'HTML',
+        'CSS',
+        'Javascript'
+      ],
+      url: 'https://movistar-planes.cl/p/movil2',
+      publishDate: new Date('2024-07-07'),
+    },
+    {
+      id: 'wow-peru',
+      title: 'Wow Perú',
+      description: 'El sitio web para Wow Perú está orientado a la captación de leads para planes móviles y servicios de internet. Los usuarios pueden explorar los planes disponibles y enviar sus datos mediante formularios. Utilicé HTML, CSS y JavaScript para desarrollar un sitio que facilita la navegación y el envío de datos.',
+      img: '/assets/wow-peru/wow-peru.png',
+      img_alt: 'Wow Perú',
+      img_mockup: '/assets/wow-peru/wow-peru-mockup.png',
+      skills: [
+        'HTML',
+        'CSS',
+        'Javascript'
+      ],
+      url: 'https://wowplanes.pe/p/td',
+      publishDate: new Date('2024-06-06'),
     },
 ];
   
